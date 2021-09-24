bd3ba66afed17bbb8492045eee70c7aed5599959
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-09-24 09:38:14 +0530

    form completed

--- fun.js
+++ fun.js
@@ -2,16 +2,14 @@
 let names=document.getElementById("name")
 let place=document.getElementById("place")
 let gender=document.getElementById("gender")
-let email=document.getElementById("email")
+let email=document.getElementById("emaill")
 let contact=document.getElementById("number")
 let statuss=document.getElementById("status")
 let education=document.getElementById("education")
 let college=document.getElementById("college")
 let year=document.getElementById("year")
-let about=document.getElementById("about")
-let why=document.getElementById("why")
-let reason=document.getElementById("reason")
-let hear=document.getElementById("hear")
+let agree=document.getElementById("agreecheck")
+
 
 function validate(){
     if(names.value=="" ){
@@ -48,17 +46,56 @@ function validate(){
             document.getElementById("s-email").innerHTML=""
             
     }
-    if(email.value=="" ){
-        document.getElementById("s-email").innerHTML="This field is required"
+    if(number.value.length!=10 ){
+        document.getElementById("s-number").innerHTML="Number must be 10 digit"
         
     }else{
         
-            document.getElementById("s-email").innerHTML=""
+            document.getElementById("s-number").innerHTML=""
+            
+    }
+    if(statuss.value=="status" ){
+        document.getElementById("s-status").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-status").innerHTML=""
+            
+    }
+    if(education.value=="education" ){
+        document.getElementById("s-education").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-education").innerHTML=""
+            
+    }
+    if(college.value=="" ){
+        document.getElementById("s-college").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-college").innerHTML=""
+            
+    }
+    if(year.value=="0" ){
+        document.getElementById("s-year").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-year").innerHTML=""
+            
+    }  if(!agree.checked){
+        document.getElementById("s-agree").innerHTML="You need to agree for registration"
+        
+    }else{
+        
+            document.getElementById("s-agree").innerHTML=""
             
     }
     
     
-    if(names.value=="" ||place.value==""||gender.value==""||gender.value=="gender"||email.value==""){
+    if(names.value=="" ||place.value==""||gender.value=="gender"||email.value==""||statuss.value=="status"||number.value.length!=10 ||education.value=="education" ||college.value=="" ||year.value=="0"||!agree.checked){
         return false
     }
     
