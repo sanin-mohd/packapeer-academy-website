e03673e20078c7ca023ff3bfeec17a5f110ed83b
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-09-24 08:35:51 +0530

    first

---
+++ fun.js
@@ -0,0 +1,65 @@
+
+let names=document.getElementById("name")
+let place=document.getElementById("place")
+let gender=document.getElementById("gender")
+let email=document.getElementById("email")
+let contact=document.getElementById("number")
+let statuss=document.getElementById("status")
+let education=document.getElementById("education")
+let college=document.getElementById("college")
+let year=document.getElementById("year")
+let about=document.getElementById("about")
+let why=document.getElementById("why")
+let reason=document.getElementById("reason")
+let hear=document.getElementById("hear")
+
+function validate(){
+    if(names.value=="" ){
+        document.getElementById("s-names").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-names").innerHTML=""
+            
+        
+    }
+    if(place.value=="" ){
+        document.getElementById("s-place").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-place").innerHTML=""
+            
+       
+    }
+     if(gender.value=="gender" ){
+        document.getElementById("s-gender").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-gender").innerHTML=""
+            
+    }
+    if(email.value=="" ){
+        document.getElementById("s-email").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-email").innerHTML=""
+            
+    }
+    if(email.value=="" ){
+        document.getElementById("s-email").innerHTML="This field is required"
+        
+    }else{
+        
+            document.getElementById("s-email").innerHTML=""
+            
+    }
+    
+    
+    if(names.value=="" ||place.value==""||gender.value==""||gender.value=="gender"||email.value==""){
+        return false
+    }
+    
+}
